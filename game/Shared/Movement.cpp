17a94809cf970c35ed52af8cdf6f552a19eefbf1
Dima <belousovdmn@gmail.com>
2023-03-04 22:24:07 -0800

    Refactored Movement to be non-inlined

--- game/Shared/Movement.cpp
+++ game/Shared/Movement.cpp
@@ -13,3 +13,39 @@ Movement::Movement()
 {}
 
 Movement::~Movement() {};
+
+bool Movement::move(glm::vec3& outPositionWorld, const float_t speed, const glm::vec3& currentPositionWorld) {
+  // Touch location coordinates
+  const float_t xScreen = xCoordinate();
+  const float_t yScreen = yCoordinate();
+  // Start moving if we actually clicked somewhere
+  if (xScreen == _defaultCoordinateVal || yScreen == _defaultCoordinateVal) {
+	if (!isTargetPositionSet()) return false;
+	const glm::vec3 cmpCurrTarget = glm::equal(currentPositionWorld, std::move(glm::vec3(targetPositionWorld.x, targetPositionWorld.y, targetPositionWorld.z)));
+	if (cmpCurrTarget.x && cmpCurrTarget.y && cmpCurrTarget.z) return false;
+	return moveInSameDirection(outPositionWorld, currentPositionWorld, speed);
+  }
+  else return moveInNewDirection(outPositionWorld, currentPositionWorld, speed, xScreen, yScreen);
+}
+
+bool Movement::moveInSameDirection(glm::vec3 &outPosition, const glm::vec3& currentPositionWorld, const float_t speed) const {
+  if (std::abs(targetPositionWorld.x - currentPositionWorld.x) <= speed && std::abs(targetPositionWorld.z - currentPositionWorld.z) <= speed)
+  {
+	outPosition = glm::vec3(targetPositionWorld);
+  }
+  else
+  {
+	// Based on: https://math.stackexchange.com/questions/3932112/move-a-point-along-a-vector-by-a-given-distance
+	const glm::vec3 directionVector = glm::vec3(targetPositionWorld.x - currentPositionWorld.x, 0.f, targetPositionWorld.z - currentPositionWorld.z);
+	outPosition = currentPositionWorld + speed * directionVector / glm::length(directionVector);
+  }
+  return true;
+}
+
+bool Movement::moveInNewDirection(glm::vec3& outPosition, const glm::vec3& currentPositionWorld, const float_t speed, const float_t xScreen, const float_t yScreen) {
+  const Math& m = Math::getInstance();
+  const Uniforms& uf = Uniforms::getInstance();
+  const glm::vec4 newPositionWorld = m.screenToWorld(xScreen, yScreen, uf.drawableWidth(), uf.drawableHeight(), uf.getViewMatrix(), uf.getProjectionMatrix());
+  targetPositionWorld = std::move(newPositionWorld);
+  return moveInSameDirection(outPosition, currentPositionWorld, speed);
+}
--- game/Shared/Movement.hpp
+++ game/Shared/Movement.hpp
@@ -23,19 +23,7 @@ public:
   Movement();
   virtual ~Movement() = 0;
   
-  inline const bool move(glm::vec3& outPositionWorld, const float_t speed, const glm::vec3& currentPositionWorld) {
-	// Touch location coordinates
-	const float_t xScreen = xCoordinate();
-	const float_t yScreen = yCoordinate();
-	// Start moving if we actually clicked somewhere
-	if (xScreen == _defaultCoordinateVal || yScreen == _defaultCoordinateVal) {
-	  if (!isTargetPositionSet()) return false;
-	  const glm::vec3 cmpCurrTarget = glm::equal(currentPositionWorld, std::move(glm::vec3(targetPositionWorld.x, targetPositionWorld.y, targetPositionWorld.z)));
-	  if (cmpCurrTarget.x && cmpCurrTarget.y && cmpCurrTarget.z) return false;
-	  return moveInSameDirection(outPositionWorld, currentPositionWorld, speed);
-	}
-	else return moveInNewDirection(outPositionWorld, currentPositionWorld, speed, xScreen, yScreen);
-  }
+  bool move(glm::vec3& outPositionWorld, const float_t speed, const glm::vec3& currentPositionWorld);
 
 private:
   const float_t _defaultCoordinateVal;
@@ -46,27 +34,9 @@ private:
 	return targetPositionWorld.x != 0.f || targetPositionWorld.y != 0.f || targetPositionWorld.z != 0.f || targetPositionWorld.w != 0.f;
   }
   
-  inline const bool moveInSameDirection(glm::vec3 &outPosition, const glm::vec3& currentPositionWorld, const float_t speed) {
-	if (std::abs(targetPositionWorld.x - currentPositionWorld.x) <= speed && std::abs(targetPositionWorld.z - currentPositionWorld.z) <= speed)
-	{
-	  outPosition = glm::vec3(targetPositionWorld);
-	}
-	else
-	{
-	  // Based on: https://math.stackexchange.com/questions/3932112/move-a-point-along-a-vector-by-a-given-distance
-	  const glm::vec3 directionVector = glm::vec3(targetPositionWorld.x - currentPositionWorld.x, 0.f, targetPositionWorld.z - currentPositionWorld.z);
-	  outPosition = currentPositionWorld + speed * directionVector / glm::length(directionVector);
-	}
-	return true;
-  }
+  bool moveInSameDirection(glm::vec3 &outPosition, const glm::vec3& currentPositionWorld, const float_t speed) const;
   
-  inline const bool moveInNewDirection(glm::vec3& outPosition, const glm::vec3& currentPositionWorld, const float_t speed, const float_t xScreen, const float_t yScreen) {
-	const Math& m = Math::getInstance();
-	const Uniforms& uf = Uniforms::getInstance();
-	const glm::vec4 newPositionWorld = m.screenToWorld(xScreen, yScreen, uf.drawableWidth(), uf.drawableHeight(), uf.getViewMatrix(), uf.getProjectionMatrix());
-	targetPositionWorld = std::move(newPositionWorld);
-	return moveInSameDirection(outPosition, currentPositionWorld, speed);
-  }
+  bool moveInNewDirection(glm::vec3& outPosition, const glm::vec3& currentPositionWorld, const float_t speed, const float_t xScreen, const float_t yScreen);
 };
 
 #endif /* Movement_h */
