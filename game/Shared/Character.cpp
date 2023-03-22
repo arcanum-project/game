f9ad70f130ebcae5886bd3a8bff7395df93b0cbf
Dima <belousovdmn@gmail.com>
2023-03-22 14:07:15 -0700

    Removed unused code

--- game/Shared/Character.cpp
+++ game/Shared/Character.cpp
@@ -16,22 +16,15 @@ Character::Character(MTL::Device * const pDevice)
   _instanceData(),
   renderingMetadata()
 {
-  populateVertexData();
   loadTextures();
 }
 
-Character::~Character() {
-  pIndexBuffer()->release();
-  pVertexBuffer()->release();
+Character::~Character()
+{
 }
 
-void Character::populateVertexData() {
-  const std::unique_ptr<const ImportedModelData> tile = ObjModelImporter().import("character-5x5", "obj");
-  setVertexData(tile->vertexData);
-  setIndices(tile->indices);
-  
-  setVertexBuffer(pDevice()->newBuffer(vertexData().data(), vertexData().size() * sizeof(VertexData), MTL::ResourceStorageModeShared));
-  setIndexBuffer(pDevice()->newBuffer(indices().data(), indices().size() * sizeof(uint16_t), MTL::ResourceStorageModeShared));
+void Character::populateVertexData()
+{
 }
 
 void Character::makeTexturesFromArt(const char * name, const char * type) {
@@ -73,22 +66,5 @@ void Character::makeTexturesFromArt(const char * name, const char * type) {
 }
 
 void Character::loadTextures() {
-//  const char* name = "ghmstxaa";
-  const char* name = "hmfc2xaa";
-//  makeTexturesFromArt(name, "art");
   makeTexturesFromArt("hmfc2xab", "art");
-//  makeTexturesFromArt("hmmrbxab", "art");
-//  makeTexturesFromArt("efmbnxab", "art");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("efmv1xaa_04", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("efmv1xaa_03", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("efmv1xaa_05", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("hmfc2xaa_05", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("efmv1xna_25", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("efmv1xna_31", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("ghmstxaa_0", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("hmfc2xai_117", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("hmfc2xai_06", "bmp");
-//    _textureIndex = TextureController::instance(pDevice()).loadTexture("hgmv1xaa_06", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("efmbnxak_80", "bmp");
-//  _textureIndex = TextureController::instance(pDevice()).loadTexture("efmbnxak_43", "bmp");
 }
